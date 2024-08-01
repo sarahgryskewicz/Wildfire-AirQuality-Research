43e7b77805c4029b649cfc7477cf70c61be6df15
sarahgryskewicz <gryskewiczs@gmail.com>
2024-08-01 10:58:11 -0600

    Update SNPP VIIRS AOD Data - Concatenating Files.py

--- CODE/SNPP VIIRS AOD Data - Concatenating Files.py
+++ CODE/SNPP VIIRS AOD Data - Concatenating Files.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## AOD Data Concatenation & Date Identification from Xarray Data
-# ### CSU REU 2024 - Sarah Gryskewicz
-# ***
-
-# In[35]:
-
-
+# Concatenate here before plotting the data (see other file)
 import xarray
 import matplotlib.pyplot as plt
 import cartopy
@@ -21,49 +12,28 @@ import os, sys, glob, os.path
 from datetime import datetime
 import pandas as pd
 
-
-# In[37]:
-
-
-snpp_direc = '/Users/C837388336/Desktop/REU/Data files/AOD/AOD nc/'
-
+# Access directory of downloaded files
+snpp_direc = '/Users/Desktop/REU/Data/AOD/AOD nc/'
 gfiles=sorted(glob.glob(snpp_direc+'AERDB_D3_VIIRS_SNPP.A2023*nc'))
-
+# Concatenate
 ds = xarray.concat([xarray.open_dataset(f) for f in gfiles],dim="time",coords="different")
 
-
-# In[38]:
-
-
 dates = []
-
 for filepath in gfiles:
-    #AERDB_D3_VIIRS_SNPP.A2023152.002.2023156000957
+    # File name example: AERDB_D3_VIIRS_SNPP.A2023152.002.2023156000957
     day_num = int(filepath[77:80]) 
     year = int(filepath[73:77])     
     date = pd.Timestamp(year=year, month=1, day=1) + pd.Timedelta(days=day_num - 1)
     # Open dataset and add date as a new coordinate
     ds = xarray.open_dataset(filepath)
     ds.coords['dates'] = date
-    #print(date)
     # Append dataset to list
     dates.append(ds)
 
 # Concatenate datasets along 'time' dimension
 ds = xarray.concat(dates, dim='time')
 
-
-# In[39]:
-
-
-ds
-
-
-# In[40]:
-
-
 #Dropping variables from a dataset: (only want a few so lots are being dropped from this file)
-#ds.drop_vars()
 vars_to_drop = ["Unsuitable_Pixel_Fraction_Land_Ocean", "Aerosol_Optical_Thickness_550_Land_Ocean_Count",\
               "Aerosol_Optical_Thickness_550_Land_Ocean_Maximum", "Aerosol_Optical_Thickness_550_Land_Ocean_Mean",\
               "Aerosol_Optical_Thickness_550_Land_Ocean_Minimum", "Aerosol_Optical_Thickness_550_Land_Ocean_Standard_Deviation",\
@@ -79,7 +49,6 @@ vars_to_drop = ["Unsuitable_Pixel_Fraction_Land_Ocean", "Aerosol_Optical_Thickne
 
 
 ds = ds.drop_vars(vars_to_drop)
-output_path = os.path.join("C:/Users/C837388336/Desktop/REU/Data files/AOD/AOD JJA", 'AOD_data_2023_JJA.nc')
-#How to save out a xarray dataset into a new netcdf:
+output_path = os.path.join("C:/Users/Desktop/REU/Data/AOD/AOD JJA", 'AOD_data_2023_JJA.nc')
+# Save out a xarray dataset into a new netcdf:
 ds.to_netcdf(output_path, format='NETCDF4')
-
